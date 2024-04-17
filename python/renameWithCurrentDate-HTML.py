1801a708732d13655a2dac5a56d4907b984011a5
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2024-04-17 11:40:53 +0300

    Update renameWithCurrentDate-HTML.py

--- python/renameWithCurrentDate-HTML.py
+++ python/renameWithCurrentDate-HTML.py
@@ -2,17 +2,24 @@ import os
 from datetime import date
 
 def main():
-	findDate = date.today()
-	today = findDate.strftime("_%d%m%y")
+    # Specify the directory path where the HTML files are located
+    dir_path = 'C:/helloWorld/VulnAssessReformat/'
+    
+    # Get the current date
+    current_date = date.today()
+    date_suffix = current_date.strftime("_%d%m%y")
 
-	dir_path = 'EDIT-DIRECTORY-PATH'
-	for filename in os.listdir(dir_path):
-		if filename.endswith('.html'):
-			size = len(filename)
-			mod = filename[:size - 12] + today + '.html'
-			src = dir_path + filename
-			dest = dir_path + mod
-			os.rename(src, dest)
+    # Iterate through the files in the directory
+    for filename in os.listdir(dir_path):
+        if filename.endswith('.html'):
+            # Construct the new filename by removing the last 24 characters and appending the date suffix
+            new_filename = filename[:-24] + date_suffix + '.html'
+            # Construct the full file paths
+            old_filepath = os.path.join(dir_path, filename)
+            new_filepath = os.path.join(dir_path, new_filename)
+            # Rename the file
+            os.rename(old_filepath, new_filepath)
+            print(f"Renamed '{filename}' to '{new_filename}'")
 
 if __name__ == '__main__':
-	main()
+    main()
